3fc7e7bc08c8ee4ca08bfc102c138287763fc66c
tfgui <tfgui2@gmail.com>
2020-08-08 20:50:38 +0900

    a

--- FSXConnectPi/FSXConnectPi.py
+++ FSXConnectPi/FSXConnectPi.py
@@ -12,35 +12,42 @@ import udp
 
 gui.running=True
 lastrendertime = 0
-while gui.running:
-    #time.sleep(0.001)
 
-    #enc1
-    dir=enc.getdirection()
-    if dir== 1:
-        print('rotate right')
-        udp.udpsend('right'.encode())
-    elif dir==-1:
-        print('left')
-        udp.udpsend('left'.encode())
-    if enc.getbutton():
-        print('press')
-        
-    #enc2
-    dir2=enc2.getdirection()
-    if dir2== 1:
-        print('2 right')
-    elif dir2==-1:
-        print('2 left')
-    if enc2.getbutton():
-        print('2 press')
-        
-    #gui event
-    if gui.getevent():
-        print('event')
-        
-    #gui render
-    rendertime = time.time()-lastrendertime
-    if rendertime>0.1:
-        gui.render()
-        lastrendertime=time.time()
+try:
+    while gui.running:
+        #time.sleep(0.001)
+
+        #enc1
+        dir=enc.getdirection()
+        if dir== 1:
+            print('rotate right')
+            udp.udpsend('right'.encode())
+        elif dir==-1:
+            print('left')
+            udp.udpsend('left'.encode())
+        if enc.getbutton():
+            print('press')
+            
+        #enc2
+        dir2=enc2.getdirection()
+        if dir2== 1:
+            print('2 right')
+        elif dir2==-1:
+            print('2 left')
+        if enc2.getbutton():
+            print('2 press')
+            
+        #gui event
+        if gui.getevent():
+            print('event')
+            
+        #gui render
+        rendertime = time.time()-lastrendertime
+        if rendertime>0.1:
+            gui.render()
+            lastrendertime=time.time()
+            
+except KeyboardInterrupt:
+    print('quit')
+    
+    
